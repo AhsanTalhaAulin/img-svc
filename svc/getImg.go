e2b28097bb8689ecbfa39c90da9cd02a4bf6022e
AhsanTalhaAulin <hoque.talha@gmail.com>
2022-12-22 17:08:36 +0600

    get request functionality implemented with uuid

--- svc/getImg.go
+++ svc/getImg.go
@@ -3,19 +3,31 @@ package svc
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 
 	"github.com/labstack/echo"
 
 	"img-svc/aws"
+	"img-svc/db"
 )
 
 func GetImg(c echo.Context) error {
 
 	name := c.QueryParam("name")
 
-	log.Println("Image name: ", name)
+	fileExt := filepath.Ext(name)
+	fileName := strings.TrimSuffix(name, fileExt)
+	log.Println("name: ", fileName, "ext name: ", fileExt)
+	uuid, err := db.GetUUID(fileName)
+	log.Println("uuid: ", uuid)
+	if err != nil {
+		log.Println(err)
+		return c.String(http.StatusBadRequest, "could not get uuid")
+	}
 
-	urlStr, err := aws.GetPresignedUrl(name)
+	log.Println(" uuid+ fileExt: ", uuid+fileExt)
+	urlStr, err := aws.GetPresignedUrl(uuid + fileExt)
 
 	if err != nil {
 		log.Println(err)
